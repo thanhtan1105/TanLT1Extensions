722b1a11ed5c6d4676eb37a88c16cec898c79004
Le Thanh Tan <thanhtan1105@gmail.com>
2016-07-17 15:14:04 +0700

    add code

---
+++ Example/TanLT1Extensions/String+Extension.swift
@@ -0,0 +1,19 @@
+//
+//  String+Extension.swift
+//  LeThanhTanFramework
+//
+//  Created by Le Thanh Tan on 7/15/16.
+//  Copyright © 2016 Le Thanh Tan. All rights reserved.
+//
+
+import Foundation
+
+extension String {
+  func replace(string:String, replacement:String) -> String {
+    return self.stringByReplacingOccurrencesOfString(string, withString: replacement, options: NSStringCompareOptions.LiteralSearch, range: nil)
+  }
+  
+  func removeWhitespace() -> String {
+    return self.replace(" ", replacement: "")
+  }
+}
