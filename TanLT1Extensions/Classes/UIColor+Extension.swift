3d6a2841dad26ca72fea4e4041167fd3d387b187
Le Thanh Tan <thanhtan1105@gmail.com>
2016-07-17 16:02:17 +0700

    add

--- TanLT1Extensions/Classes/UIColor+Extension.swift
+++ TanLT1Extensions/Classes/UIColor+Extension.swift
@@ -9,15 +9,11 @@
 import UIKit
 
 extension UIColor {
-  convenience public init(hex: Int, alpha: CGFloat = 1.0) {
+  public convenience init(hex: Int, alpha: CGFloat = 1.0) {
     let red = CGFloat((hex & 0xFF0000) >> 16) / 255.0
     let green = CGFloat((hex & 0xFF00) >> 8) / 255.0
     let blue = CGFloat((hex & 0xFF)) / 255.0
     self.init(red:red, green:green, blue:blue, alpha:alpha)
-  }
-  
-  public struct appColor {
-    public static let primaryColor = UIColor(hex: 0x4bc2ec)
   }
 }
 
